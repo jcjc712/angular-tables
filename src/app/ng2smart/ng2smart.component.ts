68a702e69079ac69af14f1a6e4f65d45f91a551e
Juan Carlos <juan.carlos@pagofacil.net>
2017-09-29 17:23:03 -0500

    creating and setting tables

---
+++ src/app/ng2smart/ng2smart.component.ts
@@ -0,0 +1,169 @@
+import { Component, OnInit } from '@angular/core';
+import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
+import {CustomRenderComponent} from './custom-render.component';
+@Component({
+  selector: 'app-ng2smart',
+  templateUrl: './ng2smart.component.html',
+  styleUrls: ['./ng2smart.component.css'],
+})
+export class Ng2smartComponent implements OnInit {
+    settings = {
+        delete: {
+            deleteButtonContent: '<i class="fa fa-pencil" style="color:white !important;" aria-hidden="true"></i>',
+            deleteButtonClass: 'btn btn-success btn-sm'
+            /*attr: {
+    class: 'btn btn-success'
+}*/
+        },
+        add: {
+            addButtonContent: 'chido'
+        },
+        actions: {
+            edit: false,
+            position: 'right'
+          //delete: false,
+        },
+        mode: 'external',
+        //hideSubHeader: true,
+        noDataMessage: 'Sin datos',
+      attr: {
+        class: 'table'
+      },
+        columns: {
+            id: {
+                title: 'ID',
+                filter: false,
+                editable: false
+            },
+            name: {
+                title: 'Full Name',
+                filter: false
+            },
+            username: {
+                title: 'Full Name',
+                type: 'custom',
+                renderComponent: CustomRenderComponent,
+            },
+            email: {
+                title: 'Email',
+                filter: false,
+                editor: {
+                    type: 'checkbox'
+                }
+            },
+            actions: {
+                title: 'Actions',
+                type: 'html'
+            }
+        }
+    };
+
+    data = [
+        {
+            id: 1,
+            name: "Leanne Graham",
+            username: "Juanchisas7",
+            email: "Sincere@april.biz",
+            actions: "<h1>Holis</h1>"
+        },
+        /*{
+            id: 2,
+            name: "Ervin Howell",
+            username: "Antonette",
+            email: "Shanna@melissa.tv"
+        },
+        {
+            id: 3,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        },
+        {
+            id: 4,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        },
+        {
+            id: 5,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        },
+        {
+            id: 6,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        },
+        {
+            id: 7,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        },
+        {
+            id: 8,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        },
+        {
+            id: 9,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        },
+        {
+            id: 10,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        },
+        {
+            id: 11,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        },
+        {
+            id: 12,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        }*/
+    ];
+
+    source: LocalDataSource; // add a property to the component
+    constructor() {
+        this.source = new LocalDataSource(this.data); // create the source
+    }
+    ngOnInit() {
+    }
+
+    onSearch(query: string = '') {
+        this.source.setFilter([
+            // fields we want to include in the search
+            {
+                field: 'id',
+                search: query
+            },
+            {
+                field: 'name',
+                search: query
+            },
+            {
+                field: 'username',
+                search: query
+            },
+            {
+                field: 'email',
+                search: query
+            }
+        ], false);
+    }
+
+    deleteItem(event) {
+        console.log(event);
+    }
+}
